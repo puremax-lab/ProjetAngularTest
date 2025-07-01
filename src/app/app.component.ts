867603d9a9bc95258098c58d9c07fce02e8042cb
puremax-lab <youtamaxime@gmail.com>
2025-07-01 13:45:46 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Bienvenu chez CFITECH';
   // imageUrl = "https://png.pngtree.com/png-clipart/20191027/ourmid/pngtree-view-icon-png-image_1869829.jpg";
-  siteUrl = "https://cfitech.be/";
-  linkText = "Visitez le site officiel de CFITECH";
+
+  // siteUrl = "https://cfitech.be/";
+
+  // linkText = "Visitez le site officiel de CFITECH";
+
+  // isDisabled = false;
+
+  // isChecked = false;
+
+  // changeTitle(){
+  //   this.title = "Bienvenu chez koekeltech";
+  // }
+
+  // prenom=""
+
+  // email=""
+  // envoyer(){
+  //   alert("Envoyez en email : "+this.email);
+  // }
+
+  // belgique = false;
+  // france = false;
+  // allemagne = false;
+  // espagne = false;
+  // italie = false;
+  // paysBas = false;
+  paysSelectionne = "";
 }
