745437da2a91c4322aafb1cd4ce81c61e1b9503b
puremax-lab <youtamaxime@gmail.com>
2025-07-04 15:59:30 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, FormsModule],
+  imports: [RouterOutlet, HomeComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Bienvenu chez CFITECH';
+  // title = 'Bienvenu chez CFITECH';
   // imageUrl = "https://png.pngtree.com/png-clipart/20191027/ourmid/pngtree-view-icon-png-image_1869829.jpg";
 
   // siteUrl = "https://cfitech.be/";
@@ -39,5 +40,43 @@ export class AppComponent {
   // espagne = false;
   // italie = false;
   // paysBas = false;
-  paysSelectionne = "";
+  // paysSelectionne = "";
+
+  // title = 'test';
+
+  // estPresent = false;// par defaut l'etudiant est absent
+
+  // marquePresent() {
+  //   this.estPresent = true; // on marque l'etudiant present
+  // }
+  // marqueAbsent() {
+  //   this.estPresent = false; // on marque l'etudiant absent
+  // }
+
+  // formateurs = [
+  //   "jean",
+  //   "Aimable",
+  //   "Julien",
+  //   "Charles",
+  //   "Pierre"
+  // ];
+
+  // role = "admin";// change cette valeur pour tester différents rôles
+
+  // title = 'Bienvenu chez CFITECH';
+  // role:string ="";
+
+  // title = "test";
+  // role="";// change cette valeur pour tester différents rôles
+
+  // title = 'Bienvenu chez CFITECH';
+  // // classCss:string="carte-formateur-stagiaire";
+  // classCss:string[]=["carte-stg-forme", "carte-stg-visuel"];
+
+  // title = 'Bienvenu chez CFITECH';
+  // estPresent = true;
+  // estAbsent = false;
+
+  title = "test";
+  couleur = "";
 }
